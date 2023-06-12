b3c506ab07a04c171b1603ea473164e091161a65
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-12 21:56:20 +0530

    user schema created for login/sign up

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+const { use } = require('../routes');
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true 
+    }
+},{
+    timestamps:true
+});
+const user=mongoose.model('user',userSchema);
+module.exports=user;
