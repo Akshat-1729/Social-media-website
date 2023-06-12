1042107746315d3f08867d675848c6fa69c1af74
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-13 00:18:01 +0530

    signup signin view created using html

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,22 @@
 module.exports.profile=function(req,res){
     return res.render('user_profile',{});
 }
+
+//render the signup page
+module.exports.signup=function(req,res){
+    return res.render('user_signup',{
+        title:"Codial | signup"
+    });
+};
+ //render the sign in page
+module.exports.signin=function(req,res){
+    return res.render('user_signin',{
+        title:"Codial | signin"
+    });
+};
+
+//get the signup data
+
+module.exports.create=function(res,req){
+    
+}
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,6 @@ const router=express.Router();
 const user_control=require('../controllers/user_controller');
 console.log('user controller loaded');
 router.get('/profile',user_control.profile);
+router.get('/sign-up',user_control.signup);
+router.get('/sign-in',user_control.signin);
 module.exports=router; 
