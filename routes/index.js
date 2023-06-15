a53d57255be1915d30271904392f0f388fc60f15
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-15 18:52:28 +0530

    saving posts to the DB with user's id

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const homeController=require('../controllers/home_controller');
 console.log("router loaded");
 router.get('/',homeController.home);
 router.use('/user',require('./user'));
+router.use('/post',require('./post'));
 //for any further routes , access from here
 //router.use('/routerName',require());
 module.exports=router;
---
+++ routes/post.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const router=express.Router();
+const post_controller=require('../controllers/post_controller');
+router.post('/create',post_controller.create);
+module.exports=router;
