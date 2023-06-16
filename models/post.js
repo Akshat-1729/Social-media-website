38e5da2d769b94d9d3065299a56b6177fc5b43cb
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-16 17:07:28 +0530

    mofided a bit

--- models/post.js
+++ models/post.js
@@ -7,6 +7,10 @@ const postSchema= new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
+    },
+    comments:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'comment'
     }
 },{timestamps:true
 });
