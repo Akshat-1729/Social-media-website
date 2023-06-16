eed091375aa6f456eb075e40bad2a5013b900de3
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-16 12:19:10 +0530

    checking authentication for creating posts

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const post_controller=require('../controllers/post_controller');
-router.post('/create',post_controller.create);
+const passport=require('../config/passport_local_strategy');
+router.post('/create',passport.checkAuthentication,post_controller.create);
 
 module.exports=router;
