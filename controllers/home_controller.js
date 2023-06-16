b14d7a5ce625b898396fa63b89fe90d3fe656113
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-16 12:10:09 +0530

    displaying the posts and its username by populating the user of each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,31 @@
 const Post=require('../models/post');
+const { populate } = require('../models/user');
 module.exports.home=function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',7); //to change the values of cookies
-    Post.find({}).
+
+    // Post.find({}).
+    // then(posts=>{
+    //     return res.render('home',{
+    //     title: "Codial | Home",
+    //     posts:posts
+    //     });
+    // }).
+    // catch(err=>{
+    //     console.log('Error in fetching posts');
+    // })
+    //populationg the user of each post
+    Post.find({}).populate('user').exec().
     then(posts=>{
         return res.render('home',{
-        title: "Codial | Home",
-        posts:posts
+            title:'Codial | Home',
+            posts:posts
         });
     })
+    .catch(err=>{
+        console.log('error in fetching posts');
+    })
+    
     
 }
 
