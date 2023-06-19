2cf4a923a88aa7d4c8d2887051a8945b75cbf3a6
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-19 18:52:28 +0530

    distribiting home in partials

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -25,3 +25,30 @@ module.exports.create = async function(req, res) {
     console.log('Error in creating comment', err);
   }
 };
+module.exports.destroy = async function (req, res) {
+    try {
+      // Find the comment and remove it from the database
+      let comment = await Comment.findOneAndDelete({
+        _id: req.params.id,
+        user: req.user.id
+      });
+   
+      if (comment) {
+        // Find the post that contains the comment
+        let post = await Post.findById(comment.post);
+  
+        if (post) {
+          // Remove the comment from the array of comments in the post
+          post.comments.pull(comment._id);
+          //save the updated post to persist changes in DB
+          await post.save();
+        }
+      }
+  
+      return res.redirect('back');
+    } catch (err) {
+      console.log('Error in destroying comment', err);
+      return res.redirect('back');
+    }
+  };
+
