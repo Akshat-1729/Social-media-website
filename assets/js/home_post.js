a221bcd9b46b085830b04c4a3730da73b0a8fb2f
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-21 13:55:43 +0530

    creating a post(recieving data)

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -10,13 +10,49 @@
                 url:'/post/create',
                 data:newPostForm.serialize(),
                 success:function(data){
-                    console.log(data);
+                    let newPost=newPostDom(data.data.post);
+                    $('#post-list>ul').prepend(newPost)
+                    
                 },
                 error:function(error){
                     console.log(error.responseText);
                 }
             })
         })
+    }
+    let newPostDom=function(i){
+        return $(`<li id="post-${i._id}">
+        <p>
+            
+            <small>
+                <a class="delete-post-button" href="/post/destroy/${i.id}">X</a>
+            </small>
+            
+            <li>${i.content}</li>
+            <br>
+            
+            <small>
+                ${i.user.name}
+                <%= i.createdAt.toLocaleString('en-US', { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'Asia/Kolkata' }) %>
+            </small>
+        </p>
+        <div class="post-comments">
+           
+                <form action="/comments/create" method="POST">
+                    <input type="text" name="content" placeholder="Type here to add comment">
+                    <input type="hidden" name="post" value="${i._id}">
+                    <button type="submit">Add Comment</button>
+                </form>
+             
+                <div class="post-comment-list">
+                    <ul id="post-comments-${i._id}">
+                        
+                    </ul>
+                </div>
+        </div>
+    </li>
+    
+        `)
     }
     createPost();
 }
