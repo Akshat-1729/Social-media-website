4a54435bafa4172fa71c41c6387b3820010fee74
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-21 17:53:31 +0530

    added noty to ajax and converted all the posts to ajax

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -13,6 +13,13 @@
                     let newPost=newPostDom(data.data.post);
                     $('#post-list>ul').prepend(newPost)
                     deletepost($('.delete-post-button',newPost))
+                    new Noty({
+                        theme:"relax",
+                        text:"Post Uploaded",
+                        type:"success",
+                        layout:'topRight',
+                        timeout:1500
+                    }).show();
                     
                 },
                 error:function(error){
@@ -63,7 +70,14 @@
                 type:'get',
                 url:$(deletelink).prop('href'),
                 success:function(data){
-                    $(`#post-${data.data.post_id}`).remove()
+                    $(`#post-${data.data.post_id}`).remove();
+                    new Noty({
+                        theme:'relax',
+                        text: 'Post Deleted',
+                        type:'success',
+                        layout:'topRight',
+                        timeout:1500
+                    }).show();
                 },
                 error:function(error){
                     console.log(error.responseText);
@@ -71,5 +85,17 @@
             })
         })
     }
+    // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+    let convertPostsToAjax=function(){
+        //itterates over every li of #post-list
+        $('#post-list>ul>li').each(function(){
+            //current is assigned the current li
+            let current=$(this);
+            //selects the .delete-post-button of current li
+            let deleteButton=$('.delete-post-button',current);
+            deletepost(deleteButton);
+        })
+    }
     createPost();
+    convertPostsToAjax();
 }
