7668f462d37f040e0532011da14d5c36313f2a85
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-07-16 17:03:15 +0530

    html template mails

--- mailers/commentsmailers.js
+++ mailers/commentsmailers.js
@@ -3,12 +3,13 @@ const nodemailer=require('../config/nodemailer');
 
 //this is another way of exporting
 exports.newComment=(comment)=>{
+    let htmlString=nodemailer.renderTemplate({comment:comment},'/comments/new_comments.ejs')
     console.log('inside new comment');
     nodemailer.transporter.sendMail({
         from:'akshatkumar7689@gmail.com',
         to:comment.user.email,
         subject:'new comment published',
-        html:'<h1>yup, your comment was published</h1>'
+        html:htmlString
     },(err,info)=>{
         if(err){console.log('error in sending mail',err);return;}
         console.log('message sent',info);
