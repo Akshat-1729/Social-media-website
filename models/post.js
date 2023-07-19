7a2c73e77db914b17dc6d90866012455c0231088
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-07-19 18:51:52 +0530

    added the likes schema and referenced them  in post and comment

--- models/post.js
+++ models/post.js
@@ -13,6 +13,12 @@ const postSchema= new mongoose.Schema({
             type:mongoose.Schema.Types.ObjectId,
             ref:'Comment'
         }
+    ],
+    likes:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Like'
+        }
     ]
 },{timestamps:true
 });
